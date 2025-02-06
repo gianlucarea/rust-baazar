3443a5e6506f7ce550ec96140c588e892ad9e7cb
Gianluca Rea <reagianluca97@gmail.com>
2025-02-06 18:14:29 +0100

    Really basic command line
    Copy Move Remove

--- rust-lang/grrs/src/main.rs
+++ rust-lang/grrs/src/main.rs
@@ -1,32 +1,27 @@
-use std::{fs::File, io::BufReader, io::BufRead};
-use clap::Parser;
-
-#[derive(Parser)]
-struct Cli {
-    command: String,
-    pattern: String,
-    path: std::path::PathBuf,
-
-}
+use std::{env::{self}, fs::{self, File}, io::{BufRead, BufReader}, path::PathBuf};
 
 fn main() {
-    let args = Cli::parse();
-    match args.command.as_str() {
-        "cat" => cat(args.path),
-        "grep" => grep(args.path,args.pattern),
-        _ => panic!()
+    let args: Vec<String> = env::args().collect();
+    let command = &args[1];
+    match command.as_str() {
+        "cat" => cat(PathBuf::from(&args[2])),
+        "grep" => grep(&args[2],PathBuf::from(&args[3])),
+        "cp" => cp(PathBuf::from(&args[2]),PathBuf::from(&args[3])),
+        "mv" => mv(PathBuf::from(&args[2]),PathBuf::from(&args[3])),
+        "rm" => rm(PathBuf::from(&args[2])),
+        _ => println!("{}", command)
     }
 }
 
-fn grep(path: std::path::PathBuf, pattern: String){
+fn grep(pattern: &String, path: std::path::PathBuf ){
     let f = File::open(&path).unwrap();
     let f = BufReader::new(f);
     for (index,line) in f.lines().enumerate() {
-        if line.unwrap().contains(&pattern){
+        if line.unwrap().contains(pattern){
             println!("Found the word {} in line {}",pattern,index);
         }   
     }
-}
+}  
 
 fn cat(path: std::path::PathBuf){
     let f = File::open(&path).unwrap();
@@ -36,3 +31,17 @@ fn cat(path: std::path::PathBuf){
      }
 }
 
+fn cp(origin: std::path::PathBuf, destination: std::path::PathBuf){
+    let content = fs::read_to_string(origin).unwrap();
+    let _ = fs::write(destination, content);
+ }
+
+ fn mv(origin: std::path::PathBuf, destination: std::path::PathBuf){
+    let content = fs::read_to_string(&origin).unwrap();
+    let _ = fs::write(destination, content);
+    let _ = fs::remove_file(&origin);
+ }
+
+ fn rm(origin: std::path::PathBuf){
+    let _ = fs::remove_file(&origin);
+ }
