1e2c377f5a4b08e20ee32ac82da3695fabe1bcfd
Gianluca Rea <reagianluca97@gmail.com>
2025-02-07 11:10:34 +0100

    Request without path variable
    GET - All runestones
    POST - One runestones

--- rust-lang/runestone/src/main.rs
+++ rust-lang/runestone/src/main.rs
@@ -1,5 +1,7 @@
-use axum::{extract::State, http::StatusCode, routing::get, Router};
-use chrono::{Date, DateTime, Utc};
+use axum::{extract::State, http::StatusCode, routing::get, Json, Router};
+use chrono::{NaiveDate, NaiveDateTime};
+use serde::{Deserialize, Serialize};
+use serde_json::json;
 use tokio::net::TcpListener;
 
 use sqlx::{postgres::PgPoolOptions, PgPool};
@@ -30,8 +32,8 @@ async fn main() {
     //Routes
     let app = Router::new()
     .route("/", get(|| async {"Hello Aldino!"}))
-    .route("runestones", get(get_all_runestones).post(create_runestone))
-    .route("runestones/{runestone_id}", get(get_runestone).patch(update_runestone).delete(delete_runestone))
+    .route("/runestones", get(get_all_runestones).post(create_runestone))
+    .route("/runestones/{runestone_id}", get(get_runestone).patch(update_runestone).delete(delete_runestone))
     .with_state(database_pool);
 
     //Run
@@ -41,6 +43,7 @@ async fn main() {
 
 }
 
+#[derive(Serialize)]
 struct Runestone {
     id: i32,
     name: String,
@@ -48,17 +51,74 @@ struct Runestone {
     inscription: Option<String>,
     magic_type: Option<String>,
     power_level: Option<i32>,
-    discovered_on: Option<DateTime<Utc>>,
+    #[serde(rename="discoveredOn")]
+    discovered_on: Option<NaiveDate>,
+    #[serde(rename="createdAt")]
+    created_at: Option<NaiveDateTime>,
+    #[serde(rename="updatedAt")]
+    updated_at: Option<NaiveDateTime>,
     location: Option<String>
 }
 
-async fn get_all_runestones(State(pg_pool): State<PgPool>) -> Result<(StatusCode,String),(StatusCode,String)>{
-    todo!()
+#[derive(Deserialize)]
+struct CreateRunestoneRequest{
+    name: String,
+    material: Option<String>,
+    inscription: Option<String>,
+    magic_type: Option<String>,
+    power_level: Option<i32>,
+    #[serde(rename="discoveredOn")]
+    discovered_on: Option<NaiveDate>,
+    location: Option<String>
 }
 
-async fn create_runestone(State(pg_pool): State<PgPool>) -> Result<(StatusCode,String),(StatusCode,String)> {
-    todo!()
+#[derive(Serialize)]
+struct CreateRunestoneRespone{
+    id: i32,
+}
 
+async fn get_all_runestones(State(pg_pool): State<PgPool>) -> Result<(StatusCode,String),(StatusCode,String)>{
+    let rows = sqlx::query_as!(Runestone,"SELECT * FROM runestones ORDER BY id")
+    .fetch_all(&pg_pool)
+    .await
+    .map_err(|e| {
+        (
+            StatusCode::INTERNAL_SERVER_ERROR,
+            json!({"success": false, "message": e.to_string()}).to_string(),
+        )
+    })?;
+    Ok((
+        StatusCode::OK,
+        json!({"success": true, "data": rows}).to_string(),
+    ))
+}
+
+async fn create_runestone(
+    State(pg_pool): State<PgPool>,
+    Json(runestone): Json<CreateRunestoneRequest>
+    ) -> Result<(StatusCode,String),(StatusCode,String)> {
+        let row = sqlx::query_as!(CreateRunestoneRespone,
+            "INSERT INTO runestones (name, material, inscription, magic_type, power_level, discovered_on, location) VALUES ($1, $2, $3, $4, $5, $6, $7) RETURNING id",
+            runestone.name,
+            runestone.material,
+            runestone.inscription,
+            runestone.magic_type,
+            runestone.power_level,
+            runestone.discovered_on,
+            runestone.location
+        )
+        .fetch_one(&pg_pool)
+        .await
+        .map_err(|e| {
+            (
+                StatusCode::INTERNAL_SERVER_ERROR,
+                json!({"success": false, "message": e.to_string()}).to_string(),
+            )
+        })?;
+        Ok((
+            StatusCode::OK,
+            json!({"success": true, "data": row}).to_string(),
+        ))
 }
 
 async fn get_runestone(State(pg_pool): State<PgPool>) -> Result<(StatusCode,String),(StatusCode,String)> {
